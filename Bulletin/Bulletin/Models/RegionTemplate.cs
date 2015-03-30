0c885c28e8edb33ced5a1c59b5997bedd7e44880
Mikhail Gersimov <mgerasim@inbox.ru>
2015-03-30 16:19:03 +1000

    territory

--- Bulletin/Bulletin/Models/RegionTemplate.cs
+++ Bulletin/Bulletin/Models/RegionTemplate.cs
@@ -5,9 +5,16 @@ using System.Web;
 
 namespace Bulletin.Models
 {
+    enum Territory {
+        Region = 1,
+        Locality = 2
+    };
     public class RegionTemplate
     {
         public virtual int ID { get; set; }        
         public virtual string Name { get; set; }
+        public virtual string Deltawind { get; set; }
+        public virtual string Deltatemperature { get; set; }
+        public virtual int Territory { get; set; }
     }
 }
